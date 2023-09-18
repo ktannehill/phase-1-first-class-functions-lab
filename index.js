f039537646cd0b2b97d47bae60db3aac17c4dc63
Kat Tannehill <katetannehill@gmail.com>
2023-09-18 08:01:07 -0600

    Update lab

--- index.js
+++ index.js
@@ -11,18 +11,26 @@ const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
 
 // circle back
 function createFareMultiplier(int) {
-    return function(fare) {
+    const fareMultiplier = function(fare) {
         return fare * int;
     }
+    return fareMultiplier
 };
 
-const fareDoubler = function(fare) {
-    return fare * 2;
-};
+// const fareDoubler = function(fare) {
+//     return fare * 2;
+// };
 
-const fareTripler = function(fare) {
-    return fare * 3;
-};
+// const fareTripler = function(fare) {
+//     return fare * 3;
+// };
+
+const fareDoubler = createFareMultiplier(2)
+fareDoubler(10)
+
+const fareTripler = createFareMultiplier(3)
+fareDoubler(10)
+
 
 function selectDifferentDrivers(arrayOfDrivers, callback) {
     return callback(arrayOfDrivers);
